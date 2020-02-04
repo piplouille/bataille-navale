b8af2790b24bc24dffc7ac70839d80d51746e1e4
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-02-04 09:33:19 +0100

    Classe Board

---
+++ README.txt
@@ -0,0 +1,5 @@
+Intallez Maven (sudo apt-get install maven)
+
+Vous pouvez lancer le projet avec la commande "mvn exec:java"
+
+J'écris dans TP/bataille-navale/src/main/java/ensta/
---
+++ TP/bataille-navale/src/main/java/ensta/Board.java
@@ -0,0 +1,12 @@
+package ensta;
+
+public class Board {
+    private String name;
+    private Character boats;
+    private boolean hits;
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
