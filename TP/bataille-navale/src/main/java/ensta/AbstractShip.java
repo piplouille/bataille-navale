37642e2157f36cdebb2aa47dc8771f7400b9fd1f
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-02-04 11:50:27 +0100

    Class AbstractShip

---
+++ TP/bataille-navale/src/main/java/ensta/AbstractShip.java
@@ -0,0 +1,28 @@
+package ensta;
+
+public enum Cardinal {
+    NORTH, EAST, SOUTH, WEST;
+}
+
+abstract class AbstractShip {
+    protected Character label;
+    protected String name;
+    protected int size;
+    protected Cardinal orientation;
+
+    public Character get_label() {return label;}
+    public String get_name() {return name;}
+    public int get_size() {return size;}
+    public Cardinal get_orientation() {return orientation;}
+
+    public void set_orientation(Cardinal n_orientation) {
+        orientation = n_orientation;
+    }
+
+    public AbstractShip(String n_name, Character n_label, int n_size, Cardinal n_orientation) {
+        name = n_name;
+        label = n_label;
+        size = n_size;
+        orientation = n_orientation;
+    }
+}
