f366cb55d75dbc23442e5251369eb8e237731649
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-02-04 11:21:08 +0100

    Print et test (Board)

--- TP/bataille-navale/src/main/java/ensta/App.java
+++ TP/bataille-navale/src/main/java/ensta/App.java
@@ -4,10 +4,8 @@ package ensta;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         Board board = new Board("cc");
         board.print();
     }
