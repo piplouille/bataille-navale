10fcce64cb5bf177d4db440451520c5c7739d9dc
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-02-11 11:32:38 +0100

    ex5 - classe ShipState done

---
+++ TP/bataille-navale/src/main/java/ensta/ship/ShipState.java
@@ -0,0 +1,36 @@
+package ensta.ship;
+
+import annexes.ColorUtil;
+import ensta.AbstractShip;
+
+public class ShipState {
+    private AbstractShip ship; // Référence vers le navire concerné par l'état
+    private boolean struck; // True si navire touché en cet endroit
+
+    public void addStrike() {
+        // Le navire est touché en cet endroit
+        struck = true;
+    }
+
+    public boolean isStruck() {
+        return struck;
+    }
+
+    public String toString() {
+        // retourne le label du navire associé : rouge si touché
+        if (isStruck()) {
+            return ColorUtil.colorize(ship.get_label(), ColorUtil.Color.RED);
+        }
+        else {
+            return ship.get_label();
+        }
+    }
+
+    public boolean isSunk() {
+        return ship.isSunk();
+    }
+
+    public AbstractShip getShip() {
+        return ship;
+    }
+}
